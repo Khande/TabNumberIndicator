781eb2901cd3b516922d774f415736cc715dab8c
khande <komisha@163.com>
2017-08-19 16:31:15 +0800

    feat(location): Add tab number indicator before file name without reality editor chaning

--- src/com/khande/idea/plugin/provider/TabNumberIndicatorProvider.java
+++ src/com/khande/idea/plugin/provider/TabNumberIndicatorProvider.java
@@ -1,6 +1,9 @@
 package com.khande.idea.plugin.provider;
 
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
 import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider;
+import com.intellij.openapi.fileEditor.impl.EditorWindow;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nullable;
@@ -10,10 +13,23 @@ import org.jetbrains.annotations.Nullable;
  */
 public class TabNumberIndicatorProvider implements EditorTabTitleProvider {
 
+    private static final Logger LOGGER = Logger.getInstance(TabNumberIndicatorProvider.class);
+
     @Nullable
     @Override
     public String getEditorTabTitle(Project project, VirtualFile file) {
-        return "test";
+        FileEditorManagerEx fileEditorManagerEx = FileEditorManagerEx.getInstanceEx(project);
+        EditorWindow currentWindow = fileEditorManagerEx.getCurrentWindow();
+        if (currentWindow != null) {
+            VirtualFile[] files = currentWindow.getFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].equals(file)) {
+                    return (i + 1) + ": " + file.getPresentableName();
+                }
+            }
+
+        }
+        return null;
     }
 
 
